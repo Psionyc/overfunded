df645725d1ca31c1e955ed909e06926e327d286d
Psyonic <102559775+Psionyc@users.noreply.github.com>
2022-12-06 20:51:52 +0100

    New Commit

--- contract/contracts/UserManager.sol
+++ contract/contracts/UserManager.sol
@@ -23,7 +23,7 @@ contract UserManager {
     }
 
     function mintPropertyNFT(uint256 _funding, string calldata _uri) public {
-        require(!users[msg.sender].fundings[_funding].property.minted, "You already minted an NFT for thus property");
+        require(!users[msg.sender].fundings[_funding].property.minted, "You already minted an NFT for this property");
         onft.safeMint(msg.sender, nextTokenId, _uri);
         nextTokenId += 1;
         users[msg.sender].fundings[_funding].property.minted = true;
